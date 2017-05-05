2eb7b0d0cc4ce4d1c63b06e80a37bcaf9e224911
Jeremy Liss <jeremyliss@gmail.com>
2017-05-05 16:44:55 +1200

    initial react setup

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const target = document.getElementById('root')
+
+  // const view = App(state)
+ReactDOM.render(<h1>BARBARA SHOP QUARTET</h1>, target)
